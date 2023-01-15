402d1efcda7a4819eeecd27e58a69c5a8d749e6b
Jim <jim@localhost.localdomain>
2023-01-15 11:36:50 -0600

    Add class that reads Ding's german-english dictionary
    Add class, GermanToEnglishDictionary, that reads the dictionary at:
    https://ftp.tu-chemnitz.de/pub/Local/urz/ding/de-en/de-en.txt.gz

---
+++ de_en/dictionary.py
@@ -0,0 +1,87 @@
+import gzip
+import magic
+import re
+
+class GermanToEnglishDictionary:
+    """
+    Loads a german-to-english dictionary from Ding's dictionary file
+    (https://www-user.tu-chemnitz.de/~fri/ding/).
+    """
+
+    def __init__(self):
+        self._german_words = {}
+
+    def add_words_from_ding_definition(definition_text, word_dictionary):
+        """ Add word definitions from a line in DING's text file. """
+
+        # Example line: 
+        #'Flugzeit {f} | Flugzeiten {pl} :: flying time | flying times'
+
+        # Ignore comment lines.
+        if re.match('\s*#', definition_text):
+            return 
+
+        # Divide line up into a list of words and a list of definitions.
+        # Strip any whitespace.
+        words, definitions = definition_text.split("::")
+        words = [word.strip() for word in words.split('|')]
+        definitions = [definition.strip() for definition in 
+            definitions.split('|')]
+
+        if len(words) != len(definitions):
+            raise ValueError('Unequal number of words and definitions in line'
+                    'for words: {}'.format(words))
+
+        for i in range(len(words)):
+
+            word_parameters = re.match(
+
+                # Get everyting up to a '{'
+                # Word entries may include space, punctuation, or addemdums.
+                '([^{]+)' 
+
+                # If there's a form, e.g. '{pl}' present, get it.
+                '(?:{([^}]+)})?', 
+
+                words[i])
+
+            if not word_parameters:
+                raise ValueError('Unable to parse line with words: {}'.format(
+                    words[i]))
+
+            word_parameters = word_parameters.groups()
+            word_key = word_parameters[0].strip()
+            word_entry = {
+                'word': word_key,
+                'definition': definitions[i]
+            }
+
+            if word_parameters[1]:
+                word_entry['form'] = word_parameters[1]
+            else:
+                word_entry['form'] = None
+
+            word_dictionary[word_key] = word_entry
+
+    def read_from_ding_file(self, *, file_name):
+        """
+        Read definitions from a dictionary in Ding's format.
+        File may be a Gzip file or plain text.
+        """
+
+        with magic.Magic(flags=magic.MAGIC_MIME_TYPE) as m:
+            if m.id_filename(file_name) == 'application/gzip':
+                dictionary_text = gzip.open(file_name, 'rt')
+            else:
+                dictionary_text = open(file_name, 'r')
+
+        for definition_text in dictionary_text:
+            GermanToEnglishDictionary.add_words_from_ding_definition(
+            definition_text, self._german_words)
+
+    def filter(self, *, key_substring):
+        """ Get an iterable filter matching the given substring. """
+
+        for key in self._german_words:
+            if key_substring in key:
+                yield self._german_words[key]
--- de_en/tests.py
+++ de_en/tests.py
@@ -1,3 +1,101 @@
 from django.test import TestCase
+from de_en.dictionary import GermanToEnglishDictionary
+from unittest.mock import MagicMock, patch, mock_open
 
 # Create your tests here.
+
+class DingDefinitionTests(TestCase):
+    """ Test reading definitions from a DING file. """
+
+    def test_reading_definition_saves_all_info(self):
+
+        word_entries = {}
+
+        GermanToEnglishDictionary.add_words_from_ding_definition(
+            'Flugzeit {f} | Flugzeiten {pl} :: flying time | flying times',
+            word_entries)
+
+        self.assertTrue('Flugzeit' in word_entries)
+        self.assertEqual(word_entries['Flugzeit']['form'], 'f')
+        self.assertEqual(word_entries['Flugzeit']['definition'], 'flying time')
+
+        self.assertTrue('Flugzeiten' in word_entries)
+        self.assertEqual(word_entries['Flugzeiten']['form'], 'pl')
+        self.assertEqual(word_entries['Flugzeiten']['definition'], 
+            'flying times')
+
+        self.assertEqual(len(word_entries), 2)
+
+    def test_read_method_reads_definition_with_addendum_after_word(self):
+
+        word_entries = {}
+
+        GermanToEnglishDictionary.add_words_from_ding_definition(
+            'das A und O [ugs.] :: the nuts and bolts [coll.]',
+            word_entries)
+
+        self.assertTrue('das A und O [ugs.]' in word_entries)
+        self.assertEqual(word_entries['das A und O [ugs.]']['definition'],
+            'the nuts and bolts [coll.]')
+        self.assertIsNone(word_entries['das A und O [ugs.]']['form'])
+
+    def test_read_method_reads_definition_with_addendum_after_form(self):
+
+        word_entries = {}
+
+        GermanToEnglishDictionary.add_words_from_ding_definition(
+            'Apfelbaum {m} [bot.] | Apfelbäume {pl} :: apple tree | apple trees',
+            word_entries)
+
+        self.assertTrue('Apfelbaum' in word_entries)
+        self.assertEqual(word_entries['Apfelbaum']['form'], 'm')
+        self.assertEqual(word_entries['Apfelbaum']['definition'], 'apple tree')
+
+        self.assertTrue('Apfelbäume' in word_entries)
+        self.assertEqual(word_entries['Apfelbäume']['form'], 'pl')
+        self.assertEqual(word_entries['Apfelbäume']['definition'], 'apple trees')
+
+    def test_read_method_ignores_comments(self):
+
+        word_entries = {}
+        GermanToEnglishDictionary.add_words_from_ding_definition(
+            '# Version :: 1.9 2020-12-22', word_entries)
+
+class GermanToEnglishDictionaryTests(TestCase):
+
+    def test_dictionary_reads_definition_from_simple_ding_file(self):
+
+        text_file_text = \
+            'Flugzeit {f} | Flugzeiten {pl} :: flying time | flying times'
+
+        dictionary = GermanToEnglishDictionary()
+
+        with patch('builtins.open', mock_open(read_data=text_file_text)) as mock_file:
+
+            dictionary.read_from_ding_file(file_name='de-en.txt')
+            mock_file.assert_called_with('de-en.txt', 'r')
+
+        word_entries = [word for word in dictionary.filter(key_substring="Flugzeit")]
+        self.assertEqual(len(word_entries), 2)
+
+        self.assertEqual(word_entries[0]['word'], 'Flugzeit')
+        self.assertEqual(word_entries[0]['form'], 'f')
+        self.assertEqual(word_entries[0]['definition'], 'flying time')
+
+        self.assertEqual(word_entries[1]['word'], 'Flugzeiten')
+        self.assertEqual(word_entries[1]['form'], 'pl')
+        self.assertEqual(word_entries[1]['definition'], 'flying times')
+
+        word_entries = [word for word in dictionary.filter(key_substring="Flugzeiten")]
+        self.assertEqual(word_entries[0]['word'], 'Flugzeiten')
+        self.assertEqual(word_entries[0]['form'], 'pl')
+        self.assertEqual(word_entries[0]['definition'], 'flying times')
+
+    def test_dictionary_reads_definitions_from_actual_ding_file(self):
+
+        dictionary = GermanToEnglishDictionary()
+        dictionary.read_from_ding_file(file_name='de_en/de-en.txt.gz')
+
+        word_entries = [word for word in dictionary.filter(key_substring="Flugzeit")]
+        self.assertGreaterEqual(len(word_entries), 1)
+
